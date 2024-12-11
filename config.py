2c09406b6bc76f1bd6cf58971a5843fe77b1aff0
cimraan khaan <103635804+cimraankhaan1@users.noreply.github.com>
2024-12-11 05:08:27 -0800

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://moaning-jillie-khaanfilms-fec81b42.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
